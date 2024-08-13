b530bf8d5e890f1c6ddadeb825c2561c72cbb3ae
Jeyd0n <polukhin.vo@icloud.com>
2024-08-13 16:00:24 +0300

    Add test set book genre

--- tests.py
+++ tests.py
@@ -13,13 +13,39 @@ class TestBooksCollector:
     def test_add_new_book_add_existed_book_len_not_change(self):
         collector = BooksCollector()
 
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book(
+            name='Гордость и предубеждение и зомби'
+        )
+        collector.add_new_book(
+            name='Гордость и предубеждение и зомби'
+        )
 
         assert len(collector.get_books_rating()) == 1
 
-    def test_(self):
-        ...
+    def test_set_book_genre_set_genre_for_each_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book(
+            name='Гордость и предубеждение и зомби'
+        )
+        collector.set_book_genre(
+            name='Гордость и предубеждение и зомби',
+            genre='Фантастика'
+        )
+
+        collector.add_new_book(
+            name='Что делать, если ваш кот хочет вас убить',
+        )
+        collector.set_book_genre(
+            name='Что делать, если ваш кот хочет вас убить',
+            genre='Детективы'
+        )
+
+        assert collector.get_book_genre(
+            name='Гордость и предубеждение и зомби'
+        ) == 'Фантастика' and collector.get_book_genre(
+            name='Что делать, если ваш кот хочет вас убить'
+        ) == 'Детективы'
 
     def test_(self):
         ...
